8fa95a062b78aa4bcbfe2ff3f434391488da0fdd
Shaun <772542545@qq.com>
2022-10-11 14:32:41 +0800

    初始化

---
+++ app/config/db.py
@@ -0,0 +1,25 @@
+
+class BliDB:
+    HOST = "127.0.0.1"
+    PORT = 5432
+    DBUSER = "postgres"
+    DBPWD = ""
+    DBNAME = "postgres"
+
+class Session:
+    HOST = "127.0.0.1"
+    PORT = 6379
+    DB = 0
+
+class CacheDF:
+    HOST = "127.0.0.1"
+    PORT = 6379
+    DB = 1
+
+class GodotDB:
+    HOST = "127.0.0.1"
+    PORT = 27017
+    USER = "root"
+    PASSWORD = ""
+    DB = "Godot"
+
